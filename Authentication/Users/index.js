b46a2a10f18e799d6674281bfd63b9a92bc92133
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-10-08 14:23:51 +0300

    set up user service

--- Authentication/Users/index.js
+++ Authentication/Users/index.js
@@ -5,21 +5,12 @@ const app = express();
 const cors = require('cors')
 
 const user = require('./routes/user');
-const projects = require('./routes/projects');
-const tasks = require('./routes/task');
-
-
-
-// const User = require('./classes/user')
 
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 app.use(cors())
 
 app.use('/users',  user)
-// app.use('/user/project' , projects)
-// app.use('/user/project/task' , tasks)
-
 
 
 
